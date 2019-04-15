427b38cae573f49a9e3d4d563c2330f6014c9701
Waldemar Leontiev <waldemar.leontiev@gmail.com>
2019-04-15 11:20:54 +0300

    Update to Swift 5.0

--- SwiftyStoreKit/PaymentsController.swift
+++ SwiftyStoreKit/PaymentsController.swift
@@ -33,9 +33,10 @@ struct Payment: Hashable {
     let simulatesAskToBuyInSandbox: Bool
     let callback: (TransactionResult) -> Void
 
-    var hashValue: Int {
-        return product.productIdentifier.hashValue
+    func hash(into hasher: inout Hasher) {
+        return product.productIdentifier.hash(into: &hasher)
     }
+    
     static func == (lhs: Payment, rhs: Payment) -> Bool {
         return lhs.product.productIdentifier == rhs.product.productIdentifier
     }
@@ -47,7 +48,7 @@ class PaymentsController: TransactionController {
 
     private func findPaymentIndex(withProductIdentifier identifier: String) -> Int? {
         for payment in payments where payment.product.productIdentifier == identifier {
-            return payments.index(of: payment)
+            return payments.firstIndex(of: payment)
         }
         return nil
     }
